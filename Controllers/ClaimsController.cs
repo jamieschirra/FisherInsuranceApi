566d7f2c3514a8d0e7f4cbdf15bbf8c2a9e287b4
jamieschirra <jamieschirra@yahoo.com>
2017-02-15 19:30:02 -0500

    added claims model

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -1,33 +1,49 @@
+
+using FisherInsuranceApi.Data;
+using FisherInsuranceApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
-[Route("api/claims/claim")]
+[Route("api/claims/")]
  public class ClaimsController : Controller
  {
+     private IMemoryStore db;
+    public ClaimsController(IMemoryStore repo)
+    {
+        db = repo;
+    } 
+
+    [HttpGet]
+    public IActionResult GetClaims()
+    {
+        return Ok(db.RetrieveAllClaims);
+    } 
+
       // POST api/claims/claim
     [HttpPost]
-    public IActionResult Post([FromBody]string value)
+    public IActionResult Post([FromBody] Claim claim)
     {
-        return Created("", value);
+        return Ok(db.CreateClaim(claim));
     }
 
     // GET api/claims/claim/5
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
-        return Ok("The id is: " + id);
+        return Ok(db.RetrieveClaim(id));
     }
 
      // PUT api/claims/claim/id
     [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody]string value)
+    public IActionResult Put(int id, [FromBody] Claim claim)
     {
-        return NoContent();
+        return Ok(db.UpdateClaim(claim));
     }
     
     // DELETE api/claims/claim/id
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
-        return Delete(id);
+        db.DeleteClaim(id);
+        return Ok();
     }
  }
