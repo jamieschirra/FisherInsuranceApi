bbc925c30406db2c98ba516706b3f4390b788923
jamieschirra <jamieschirra@yahoo.com>
2017-03-28 21:19:10 -0400

    Lab sevennnnnnnnnnnn

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -1,26 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from "./auth.service";
 import 'rxjs/Rx';
-// import our application component
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component'; 
+import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClaimsComponent } from './components/claims/claims.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+
+import { AppRouting } from './app.routing';
+import { AuthHttp } from "./auth.http";
 
 @NgModule({
- declarations: [
- AppComponent,
- HomeComponent,
- NavBarComponent
- ],
- imports: [
- BrowserModule,
- HttpModule
- ],
- providers: [ 
- ],
- bootstrap: [
- AppComponent
- ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        NavBarComponent,
+        LoginComponent,
+        ClaimsComponent,
+        QuotesComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AppRouting
+    ],
+    providers: [
+        AuthService
+    ],
+    bootstrap: [
+        AppComponent,
+        AuthHttp
+    ],
 })
-export class AppModule { } 
+export class AppModule { }
